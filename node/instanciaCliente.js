dcae5927d394f1291ab07b02026c3a9414b175ae
João Pedro <jpedrodaibello1999@outlook.com>
2020-05-26 16:51:11 -0300

    Node: Instância Única vs Nova Instância

---
+++ node/instanciaCliente.js
@@ -0,0 +1,13 @@
+const contadorA = require('./instanciaUnica')
+const contadorB = require('./instanciaUnica')
+
+const contadorC = require('./instanciaNova')()
+const contadorD = require('./instanciaNova')()
+
+contadorA.inc()
+contadorA.inc()
+console.log(contadorA.valor, contadorB.valor)
+
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor, contadorD.valor) // Difere porque são instancias novas criadas a partir de uma factory
